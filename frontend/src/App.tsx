809ea5d84ddee001de17e04d90beb4517537a70f
Rohit0265 <rohitmathur05458@gmail.com>
2026-02-28 16:18:29 +0530

    rohit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Fotter from "./components/Fotter"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Fotter/>
+    </div>
+  )
+}
+
+export default App
